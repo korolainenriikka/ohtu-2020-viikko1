ae9172e1f1e5cdefddd9c661c873cadb05364992
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-10-27 19:16:22 +0200

    fixed tests

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -61,7 +61,7 @@ public class Varasto {
     }
 
     public double otaVarastosta(double maara) {
-        if (maara > 0) // virhetilanteessa voidaan tehdä 
+        if (maara < 0) // virhetilanteessa voidaan tehdä 
         {
             return 0.0;   // tällainen pikapoistuminenkin!
         }
